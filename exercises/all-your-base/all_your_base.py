69a180aa560128bc91ae73210a4378211e7b14d8
iOne <rukerower@gmail.com>
2018-10-22 01:24:33 +0500

    not work yet

--- exercises/all-your-base/all_your_base.py
+++ exercises/all-your-base/all_your_base.py
@@ -1,2 +1,28 @@
-def rebase(input_base, digits, output_base):
-    return int(int(digits, base=input_base), base=output_base)
+def rebase(input_base, digits, output_base):           # TODO make it work!
+    if not input_base or not output_base:
+        raise ValueError('something go wrong')
+    d = ''
+    for i in digits:
+        if 0 <= i < 10:
+            d += str(i)
+        elif i == 10:
+            d += 'a'
+        elif i == 11:
+            d += 'b'
+        elif i == 12:
+            d += 'c'
+        elif i == 13:
+            d += 'd'
+        elif i == 14:
+            d += 'e'
+        elif i == 15:
+            d += 'f'
+        else:
+            raise ValueError('something fo wrong')
+    in_dec = int(d, base=input_base)
+    result = []
+    while in_dec:
+        in_dec, r = divmod(in_dec, output_base)
+        result.append(r)
+    result.reverse()
+    return result
